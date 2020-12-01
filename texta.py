d0f6f07dd1e6c1ff2f73a2164ea6771487ec79d3
Marta Brüning <marta@Martas-Air.fritz.box>
2020-12-01 16:45:25 +0100

    reanrranged thread top of main.

--- texta.py
+++ texta.py
@@ -7,7 +7,12 @@ from Reader import Reader
 from Keyboard import Keyboard
 
 def main():
-
+     #Setting up threads
+    thread_lock = threading.Lock()
+    keyboard = Keyboard(3, 'keyboard')
+    writer = Writer(1, 'writer', connection, thread_lock, keyboard)
+    reader = Reader(2, 'reader', connection, thread_lock)
+    
     # Connecting to LoRa mcu.
     connection = Connection('/dev/ttyS0', 115200, 8, 'N', 1, 1)
     connection.connect_device()
@@ -24,11 +29,7 @@ def main():
     configure.config_modul('AT+SAVE')
     configure.read_from_mcu()
 
-    #Setting up threads
-    thread_lock = threading.Lock()
-    keyboard = Keyboard(3, 'keyboard')
-    writer = Writer(1, 'writer', connection, thread_lock, keyboard)
-    reader = Reader(2, 'reader', connection, thread_lock)
+   
 
     writer.start()
     reader.start()
