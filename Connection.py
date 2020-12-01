e157cd4509145fbb55e307ec3096f86d6a331cb5
Marta Brüning <marta@Martas-Air.fritz.box>
2020-12-01 17:18:42 +0100

    reanrranged thread top of main.

--- Connection.py
+++ Connection.py
@@ -26,4 +26,4 @@ class Connection:
     def read_from_mcu(self):
         message = self.serial_connection.readline()
        # print(message.decode('utf-8').split(b'\r\n'))
-        print(message.decode())
+        print(str(message, 'utf-8'))
