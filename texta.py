fdf3bed6f675f07ff6d28bcafad8199a9223990d
Marta Brüning <marta@Martas-Air.fritz.box>
2020-12-01 17:28:51 +0100

    reanrranged thread top of main.

--- texta.py
+++ texta.py
@@ -19,9 +19,11 @@ def main():
     writer = Writer(1, 'writer', connection, thread_lock, keyboard)
     reader = Reader(2, 'reader', connection, thread_lock)
     
+    writer.start()
+    reader.start()
+    
     configure = Configuration(writer)
     configure.config_modul('AT+RST')
-    time.sleep(2)
     connection.read_from_mcu()
     configure.config_modul('AT+CFG=433000000,20,6,12,1,1,0,0,0,0,3000,8,4')
     time.sleep(2)
@@ -36,8 +38,7 @@ def main():
     time.sleep(2)
     connection.read_from_mcu()
 
-    writer.start()
-    reader.start()
+    
 
 if __name__ == '__main__':
     main()
