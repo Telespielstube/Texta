9cc393bbe524220c1c79e630c691aa07a141aea9
Marta <martabruening@icloud.com>
2021-01-15 15:46:50 +0100

    changed nums in splittet field parser class.

--- Parser.py
+++ Parser.py
@@ -15,18 +15,18 @@ class Parser():
     # field[0] = source adress, field[1] = flag, field [2] = ttl
     def parse_header(self, protocol_header, neighbor_node):
         protocol_field = protocol_header.split(b'|')
-        if protocol_field[1] == b'1':
+        if protocol_field[2] == b'1':
             # destination = protocol_field[3], next_node = protocol_field[4], payload = protocol_field[5]
-            self.writer.forward_message(TextMessage(protocol_field[0], protocol_field[1], protocol_field[2], protocol_field[3], protocol_field[4], protocol_field[5]))
-        if protocol_field[1] == b'3':
+            self.writer.forward_message(TextMessage(protocol_field[1], protocol_field[2], protocol_field[3], protocol_field[4], protocol_field[5], protocol_field[6]))
+        if protocol_field[2] == b'3':
             # requested_node = protocol_field[4]
-            self.writer.route_request(RouteRequest(protocol_field[0], protocol_field[1], protocol_field[2], int(protocol_field[3]), protocol_field[4]), neighbor_node)
-        if protocol_field[1] == b'4':
+            self.writer.route_request(RouteRequest(protocol_field[1], protocol_field[2], protocol_field[3], int(protocol_field[4]), protocol_field[5]), neighbor_node)
+        if protocol_field[2] == b'4':
             # end_node = protocol_field[4], next_node = protocol_field[5]
-            self.writer.route_reply(RouteReply(protocol_field[0], protocol_field[1], protocol_field[2], int(protocol_field[3]), protocol_field[4], protocol_field[5]), neighbor_node)
-        if protocol_field[1] == b'5':
+            self.writer.route_reply(RouteReply(protocol_field[1], protocol_field[2], protocol_field[3], int(protocol_field[4]), protocol_field[5], protocol_field[6]), neighbor_node)
+        if protocol_field[2] == b'5':
             #broken_node = protocol_field[3]
-            self.writer.route_error(RouteError(protocol_field[0], protocol_field[1], protocol_field[2], protocol_field[3]))
+            self.writer.route_error(RouteError(protocol_field[1], protocol_field[2], protocol_field[3], protocol_field[4]))
 
     # Parses incoming byte stream. 
     # @mcu_header       mcu message part.
