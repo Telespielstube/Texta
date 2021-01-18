8628927fcf33cb6dabca9d1676bee67b3853f550
Marta Brüning <marta@Martas-Air.fritz.box>
2021-01-18 13:39:32 +0100

    bugfix.

--- Writer.py
+++ Writer.py
@@ -42,7 +42,7 @@ class Writer(threading.Thread):
             if not self.routing_table.search_entry(request.source): 
                 self.routing_table.add_route_to_table(request.source, neighbor_node, request.hop)
             if self.routing_table.search_entry(request.requested_node):  
-                self.send_message(message_to_string(RouteReply(request.source, 4, 9, 0, request.requested_node, neighbor_node)))
+                self.send_message(self.message_to_string(RouteReply(request.source, 4, 9, 0, request.requested_node, neighbor_node)))
             elif request.decrement_time_to_live() > 0:  
                 request.increment_hop(request.hop)
                 self.send_message(self.message_to_string(request))
