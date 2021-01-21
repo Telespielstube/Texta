6238da1e048867a067626b60ebf5f61e2b844e05
Marta <martabruening@icloud.com>
2021-01-21 10:52:59 +0100

    edited reset lora.

--- Configuration.py
+++ Configuration.py
@@ -11,11 +11,11 @@ class Configuration:
     def config_module(self, *arguments):
         self.connection.write_to_mcu(arguments[0])
         for i in range(0, 1):        
-            time.sleep(1)
+            time.sleep(2)
             validation = self.connection.read_from_mcu()
             print(validation[:-1].decode())
         for i in range(1, len(arguments)):
             self.connection.write_to_mcu(arguments[i])
-            time.sleep(1)       
+            time.sleep(2)       
             validation_from_mcu = self.connection.read_from_mcu()
             print(validation_from_mcu[:-1].decode())
