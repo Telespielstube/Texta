7ac6d17f668c91decfe3d2afb524cc25fdb88a47
Marta Brüning <marta@Martas-Air.fritz.box>
2021-01-27 15:04:55 +0100

    adding some user information to the main.py.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import threading, sys
+import threading, sys, time
 
 from Connection import Connection
 from Configuration import Configuration    
@@ -10,11 +10,23 @@ from MessageHandler import MessageHandler
 from UserInterface import UserInterface
 from RoutingTable import RoutingTable
 
+def process_argument():
+    MY_ADDRESS = ''
+    if sys.argv[1]:
+        MY_ADDRESS = str(sys.args[1])
+    else:
+        print('Application expects one address parameter to run:')
+        print('$>python3 main.py [xxxx]. xxxx adress range from 0130 - 0140.')
+        sys.exit(0)
+    return MY_ADDRESS
+
 def main(argv):
-    MY_ADDRESS = str(sys.argv[1])  
+    MY_ADDRESS = process_argument(argv)
+    print('Minimalistic chat application that uses a LoRa modul for communication.\n Type \'SEND\' to send a text(max. 240 Characters) or type \'USER\' to get an overview of all available chat peers.')
+    time.sleep(5)
     #connection = Connection('/dev/ttyS0', 115200, 8, 'N', 1, 5)
     connection = Connection('/dev/ttys004', 115200, 8, 'N', 1, 5)
-    connection.connect_device()
+    connection.connect_device() 
     configuration = Configuration(connection, sys.argv[1])
     #configuration.config_module('AT+RST', 'AT+CFG=433500000,5,9,7,1,1,0,0,0,0,3000,8,4', 'AT+ADDR=' + MY_ADDRESS, 'AT+DEST=FFFF', 'AT+RX', 'AT+SAVE')
     routing_table = RoutingTable(MY_ADDRESS.encode(), 0) #Adds own address and 0 hops to routing table
