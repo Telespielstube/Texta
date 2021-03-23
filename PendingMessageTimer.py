20c28181b507ac9b599adcf7b9ab990403f71e49
Marta Brüning <marta@icloud.com>
2021-03-23 20:11:47 +0100

    Increased waiting time for cleanup to 10 sec.

--- PendingMessageTimer.py
+++ PendingMessageTimer.py
@@ -10,7 +10,7 @@ class PendingMessageTimer(threading.Thread):
 
     def run(self):
         while True:
-            time.sleep(8.0)
+            time.sleep(10.0)
             if self.message_handler.pending_message_list:
                 self.message_handler.clean_up_pending_message_list()
             if self.message_handler.route_ack_list:
