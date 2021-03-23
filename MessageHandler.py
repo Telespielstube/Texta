827f3e9de801e6d29283750407a19464a65d5583
Marta Brüning <marta@icloud.com>
2021-03-23 16:00:33 +0100

    moved search_entry in reply func to beginning of if statement.

--- MessageHandler.py
+++ MessageHandler.py
@@ -68,7 +68,7 @@ class MessageHandler:
                 reply.increment_hop()
                 self.routing_table.add_route_to_table(reply.source, neighbor_node, reply.hop)
                 self.process_pending_user_message()             
-        if reply.next_node == self.MY_ADDRESS and not self.routing_table.search_entry(reply.source) and reply.end_node != self.MY_ADDRESS:
+        if not self.routing_table.search_entry(reply.source) and reply.next_node == self.MY_ADDRESS and reply.end_node != self.MY_ADDRESS:
             reply.increment_hop() 
             self.routing_table.add_route_to_table(reply.end_node, neighbor_node, reply.hop)
             if reply.decrement_time_to_live() > 0:                 
